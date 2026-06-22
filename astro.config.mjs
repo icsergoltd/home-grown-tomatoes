9dc5327576f4c0b2e9b57396ca7551272dea5e7f
icsergoltd <icsergoltd@gmail.com>
2026-06-22 21:46:18 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://growtomatoesathome.com',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
